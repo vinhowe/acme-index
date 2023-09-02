ee8a2d0395b5c5ddd398600021f0cd7fb55ba3bc
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2023-09-01 23:14:06 -0600

    Use 127.0.0.1 because of ipv6 issues

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 const LOGIN_URL =
   `${process.env.NEXT_PUBLIC_WORKER_URL}/auth/login` ||
-  "http://localhost:8787/auth/login";
+  "http://127.0.0.1:8787/auth/login";
 
 const RedirectComponent = () => {
   useEffect(() => {
