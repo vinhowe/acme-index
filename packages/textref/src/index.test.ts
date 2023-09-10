5ee3697e7be92a709c72946736dd1b8e233b41a0
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2023-09-09 20:29:24 -0600

    Add tests for partial parsing

--- packages/textref/src/index.test.ts
+++ packages/textref/src/index.test.ts
@@ -1,6 +1,6 @@
 import { parseRef } from "./index";
 
-test("Full example", () => {
+test("Exact parsing", () => {
   expect(parseRef("acme:v1/text/1.1.3")).toMatchObject({
     namespace: "acme",
     book: "v1",
@@ -111,5 +111,122 @@ test("Full example", () => {
     listItemEnd: undefined,
     listItemRangeStart: "ii",
     listItemRangeEnd: "xv",
+  });
+});
+
+test("Partial parsing", () => {
+  expect(parseRef("acme:v1/text/1.1.3", { partial: true })).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "1",
+    section: "1",
+    subsection: "3",
+    listItemStartRangeStart: undefined,
+    listItemStartRangeEnd: undefined,
+    chapterEnd: undefined,
+    sectionEnd: undefined,
+    subsectionEnd: undefined,
+    listItemEndRangeStart: undefined,
+    listItemEndRangeEnd: undefined,
+  });
+  expect(parseRef("acme:v1/text/1", { partial: true })).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "1",
+    section: undefined,
+    subsection: undefined,
+    listItemStartRangeStart: undefined,
+    listItemStartRangeEnd: undefined,
+    chapterEnd: undefined,
+    sectionEnd: undefined,
+    subsectionEnd: undefined,
+    listItemEndRangeStart: undefined,
+    listItemEndRangeEnd: undefined,
+  });
+
+  expect(parseRef("acme:v1/text/1.2", { partial: true })).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "1",
+    section: "2",
+    subsection: undefined,
+    listItemStartRangeStart: undefined,
+    listItemStartRangeEnd: undefined,
+    chapterEnd: undefined,
+    sectionEnd: undefined,
+    subsectionEnd: undefined,
+    listItemEndRangeStart: undefined,
+    listItemEndRangeEnd: undefined,
+  });
+
+  expect(parseRef("acme:v1/text/A.2", { partial: true })).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "A",
+    section: "2",
+    subsection: undefined,
+    listItemStartRangeStart: undefined,
+    listItemStartRangeEnd: undefined,
+    chapterEnd: undefined,
+    sectionEnd: undefined,
+    subsectionEnd: undefined,
+    listItemEndRangeStart: undefined,
+    listItemEndRangeEnd: undefined,
+  });
+
+  expect(
+    parseRef("acme:v1/text/1.1.3(ii)..2.1.1(iii)", { partial: true }),
+  ).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "1",
+    section: "1",
+    subsection: "3",
+    listItemStartRangeStart: "ii",
+    listItemStartRangeEnd: undefined,
+    chapterEnd: "2",
+    sectionEnd: "1",
+    subsectionEnd: "1",
+    listItemEndRangeStart: "iii",
+    listItemEndRangeEnd: undefined,
+  });
+
+  expect(parseRef("acme:v1/text/1.1.3(ii)", { partial: true })).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "1",
+    section: "1",
+    subsection: "3",
+    listItemStartRangeStart: "ii",
+    listItemStartRangeEnd: undefined,
+    chapterEnd: undefined,
+    sectionEnd: undefined,
+    subsectionEnd: undefined,
+    listItemEndRangeStart: undefined,
+    listItemEndRangeEnd: undefined,
+  });
+
+  expect(
+    parseRef("acme:v1/text/1.1.3(ii..xv)", { partial: true }),
+  ).toMatchObject({
+    namespace: "acme",
+    book: "v1",
+    type: "text",
+    chapter: "1",
+    section: "1",
+    subsection: "3",
+    listItemStartRangeStart: "ii",
+    listItemStartRangeEnd: "xv",
+    chapterEnd: undefined,
+    sectionEnd: undefined,
+    subsectionEnd: undefined,
+    listItemEndRangeStart: undefined,
+    listItemEndRangeEnd: undefined,
   });
 });
