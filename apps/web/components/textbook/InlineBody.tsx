3202ae10daeeb5d0e28ea44036f1164a02a5d10a
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2023-09-18 12:50:21 -0600

    Make InlineBody a forwardRef to fix annoying error

--- apps/web/components/textbook/InlineBody.tsx
+++ apps/web/components/textbook/InlineBody.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { InlineItem } from "@acme-index/common";
 import MathRender from "./MathRender";
 import PageBreak from "./PageBreak";
@@ -8,22 +8,24 @@ export interface InlineBodyProps {
   items: Array<InlineItem>;
 }
 
-const InlineBody: React.FC<InlineBodyProps> = ({ items }) => {
-  return (
-    <p className="text-justify">
-      {items.map((item, index) => {
-        if (item.type === "inline") {
-          return <MathRender key={index} body={item.body} />;
-        } else if (item.type === "pagebreak") {
-          return <PageBreak key={index} page={item.page} inline />;
-        } else if (item.type === "reference") {
-          return <InlineReference key={index} item={item} />;
-        } else {
-          return null;
-        }
-      })}
-    </p>
-  );
-};
+const InlineBody = forwardRef<HTMLParagraphElement, InlineBodyProps>(
+  function InlineBody({ items }: InlineBodyProps, ref) {
+    return (
+      <p className="text-justify" ref={ref}>
+        {items.map((item, index) => {
+          if (item.type === "inline") {
+            return <MathRender key={index} body={item.body} />;
+          } else if (item.type === "pagebreak") {
+            return <PageBreak key={index} page={item.page} inline />;
+          } else if (item.type === "reference") {
+            return <InlineReference key={index} item={item} />;
+          } else {
+            return null;
+          }
+        })}
+      </p>
+    );
+  },
+);
 
 export default InlineBody;
