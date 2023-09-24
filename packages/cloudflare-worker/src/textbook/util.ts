4c7b56b95069cad9390a8d27b2369bc949f4b687
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2023-09-24 16:16:25 -0600

    Add option for http textbook fileserver

--- packages/cloudflare-worker/src/textbook/util.ts
+++ packages/cloudflare-worker/src/textbook/util.ts
@@ -2,7 +2,13 @@ import { Octokit } from '@octokit/rest';
 import { Env } from '../types';
 import { BaseChapter, parseTextbook } from '@acme-index/common';
 
-export const getTextbook = async <T extends BaseChapter>(book: string, name: string, env: Env, botOctokit: Octokit) => {
+const getTextbookLocal = async <T extends BaseChapter>(book: string, name: string, env: Env, botOctokit: Octokit) => {
+  const response = await fetch(`${env.TEXT_REPOSITORY}/${name}.md`);
+  const text = await response.text();
+  return await parseTextbook<T>('acme', book, text);
+};
+
+const getTextbookKV = async <T extends BaseChapter>(book: string, name: string, env: Env, botOctokit: Octokit) => {
   let textbookData: any;
   const kvValue = await env.TEXTBOOK.get(name, 'json');
   if (!kvValue) {
@@ -21,4 +27,13 @@ export const getTextbook = async <T extends BaseChapter>(book: string, name: str
     textbookData = kvValue as Record<string, T>;
   }
   return textbookData;
+};
+
+export const getTextbook = async <T extends BaseChapter>(book: string, name: string, env: Env, botOctokit: Octokit) => {
+  // Check if env.TEXT_REPOSITORY is a URL (for local development)
+  if (env.TEXT_REPOSITORY.startsWith('http://') || env.TEXT_REPOSITORY.startsWith('https://')) {
+    return await getTextbookLocal<T>(book, name, env, botOctokit);
+  } else {
+    return await getTextbookKV<T>(book, name, env, botOctokit);
+  }
 };
